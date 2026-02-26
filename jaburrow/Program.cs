e2cd601aa59beed0b7ee44589aeb753db04f1e90
JulianAburrow <75375767+JulianAburrow@users.noreply.github.com>
2026-02-26 16:50:12 +0000

    Feature/add google analytics (#7)
    Added Google Analytics to the application
    
    ---------
    
    Co-authored-by: Julian Aburrow <vdub1992@hotmail.co.uk>

--- jaburrow/Program.cs
+++ jaburrow/Program.cs
@@ -1,4 +1,5 @@
 using jaburrow;
+using jaburrow.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -10,5 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped<NavigationTracker>();
 
 await builder.Build().RunAsync();
---
+++ jaburrow/wwwroot/ga4.d.ts
@@ -0,0 +1,12 @@
+﻿// Declare the GA4 dataLayer on the Window object
+interface Window {
+    dataLayer: any[];
+    blazorGtag: (...args: any[]) => void;
+}
+
+// Declare the global gtag() function used by GA4
+declare function gtag(
+    command: string,
+    param1?: any,
+    param2?: any
+): void;
